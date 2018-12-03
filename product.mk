150cb84bfb3f105c99ea99fc4e236d39fadd4762
Ian Roy <ian.t.roy@gmail.com>
2018-12-03 06:55:25 -0500

    Updating to match upstream

--- product.mk
+++ product.mk
@@ -1,22 +1,21 @@
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
-PRODUCT_PACKAGES += auditd
 
 $(call prepend-product-if-exists, vendor/EnderBlue/product.mk)
 
 ifneq ($(filter lineage_marlin lineage_mata lineage_nash lineage_sailfish,$(TARGET_PRODUCT)),)
-# TARGET_ARCH doesn't get set in time, so let's make sure its done before we inherit gapps
-TARGET_ARCH := arm64
--include vendor/gapps/$(TARGET_ARCH)/$(TARGET_ARCH)-vendor.mk
-endif
-
-TARGET_EXTRA_KERNEL_MODULES := wireguard
+GAPPS_VARIANT := nano
+$(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
 
 PRODUCT_COPY_FILES += \
-    vendor/extra/rootdir/etc/init/ttl.rc:system/etc/init/ttl.rc
+  vendor/opengapps/sources/arm/lib/28/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+  vendor/opengapps/sources/arm/lib/28/libsketchology_native.so:system/lib/libsketchology_native.so \
+  vendor/opengapps/sources/arm64/lib64/28/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so \
+  vendor/opengapps/sources/arm64/lib64/28/libsketchology_native.so:system/lib64/libsketchology_native.so
+endif
 
-# Themes
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-PRODUCT_PROPERTY_OVERRIDES += ro.boot.vendor.overlay.theme=com.google.android.theme.pixel
-PRODUCT_PROPERTY_OVERRIDES += ro.com.google.ime.theme_id=5
-PRODUCT_PACKAGES += \
-    PixelTheme
+$(shell sh vendor/extra/get-blacklist-hosts-file.sh)
+
+PRODUCT_COPY_FILES += \
+    vendor/extra/rootdir/etc/hosts:system/etc/hosts \
+    vendor/extra/rootdir/etc/init/ttl.rc:system/etc/init/ttl.rc
+    
